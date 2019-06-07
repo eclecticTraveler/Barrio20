64c009b16e63544ddfc4957d9d0ef8cac8285656
Alberto Arellano <are11001@byui.edu>
2019-06-07 00:02:40 -0600

    Adding Moldels and bootstrap first bare bone frames

---
+++ RexburgBarrio20/RexburgBarrio20/Models/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RexburgBarrio20.Models
+{
+    public class Address
+    {
+        public string MainAddress { get; set; }
+        public string UnformattedAddress { get; set; }
+        public string FormattedAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+
+
+    }
+}
